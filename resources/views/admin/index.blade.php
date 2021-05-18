931910eb869792a47f9e63cfa4d8cccf2774e701
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2021-05-18 16:24:45 +0700

    Posts and Admin Page | Setting page views, Menu links, Migration

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,5 +1,7 @@
-@extends('layouts.admin')
+<x-admin-master>
 
-@section('content')
-    <h1>Admin</h1>
-@endsection
+    @section('content')
+        <h1 class="h3 mb-4 text-gray-800">Admin</h1>
+    @endsection
+
+</x-admin-master>
--- routes/web.php
+++ routes/web.php
@@ -14,14 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('/admin', function () {
-    return view('admin.index');
-});
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
